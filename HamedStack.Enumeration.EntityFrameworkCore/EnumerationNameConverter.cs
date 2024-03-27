74d9190fcfb0e859000392067e86584849f64ed5
hamedfathi <hamedfathi@outlook.com>
2024-03-27 09:12:32 +0100

    feat(project): source code added.

---
+++ HamedStack.Enumeration.EntityFrameworkCore/EnumerationNameConverter.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace HamedStack.TheEnumeration.EntityFrameworkCore;
+
+public class EnumerationNameConverter<TEnumeration> : ValueConverter<TEnumeration, string>
+    where TEnumeration : Enumeration<TEnumeration>
+{
+    public EnumerationNameConverter() : base(
+        v => v.Name,
+        v => Enumeration<TEnumeration>.FromName(v))
+    {
+    }
+}
---
+++ HamedStack.Enumeration.EntityFrameworkCore/EnumerationValueConverter.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace HamedStack.TheEnumeration.EntityFrameworkCore;
+
+public class EnumerationValueConverter<TEnumeration> : ValueConverter<TEnumeration, long>
+    where TEnumeration : Enumeration<TEnumeration>
+{
+    public EnumerationValueConverter() : base(
+        v => v.Value,
+        v => Enumeration<TEnumeration>.FromValue(v))
+    {
+    }
+}
